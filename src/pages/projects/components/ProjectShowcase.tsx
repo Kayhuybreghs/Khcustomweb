c877a078333b890cbce415b151999010ee1b2c9d
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-17 23:28:18 +0200

    Fix image loading issues

--- src/pages/projects/components/ProjectShowcase.tsx
+++ src/pages/projects/components/ProjectShowcase.tsx
@@ -52,7 +52,7 @@ const ProjectShowcase: React.FC = () => {
           description="Een moderne website voor een lokaal installatiebedrijf, met focus op duidelijke diensten en een gebruiksvriendelijk contactformulier."
           duration="1 week"
           image="/Website wemade/Opmaat-Webdesign.webp"
-          technologies={['React', 'TailwindCSS', 'SEO', 'Responsive', 'HTML', 'Java Script']}
+          technologies={['React', 'TailwindCSS', 'SEO', 'Responsive', 'HTML', 'JavaScript']}
         />
         
         <ProjectCard 
@@ -60,7 +60,7 @@ const ProjectShowcase: React.FC = () => {
           description="Een dynamische website voor een textieldrukkerij, inclusief online offerteaanvraag en Statistieken Dashboard."
           duration="5 dagen"
           image="/Website wemade/Kleding-bedrukken-SPWEAR.webp"
-          technologies={['React', 'TailwindCSS', 'Java Script', 'Forms', 'HTML', 'Statistieken Dashboard']}
+          technologies={['React', 'TailwindCSS', 'JavaScript', 'Forms', 'HTML', 'Statistieken Dashboard']}
         />
       </div>
     </>
