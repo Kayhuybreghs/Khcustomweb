882d02279d1377285e670947329185501eaacd6d
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-17 22:58:38 +0200

    Optimize mobile experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Optional: import vite-plugin-pwa if you want PWA support
-// import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -11,26 +8,20 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    // Enable minification for production
     minify: 'esbuild',
     cssCodeSplit: true,
-    // Enable modern build targeting modern browsers
     modernize: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor' // vendor.js bevat externe libraries
+          }
+        }
+      }
+    }
   },
   server: {
-    // Specify the port number, if needed
     port: 3000,
-  },
-  // Uncomment the following to enable PWA if you want to use Service Workers and caching
-  // pwa: {
-  //   registerType: 'autoUpdate',
-  //   workbox: {
-  //     runtimeCaching: [
-  //       {
-  //         urlPattern: /\/$/,
-  //         handler: 'NetworkFirst',
-  //       },
-  //     ],
-  //   },
-  // },
+  }
 })
