5e20b3d823eed29f3284b1a0ef5af9efae224543
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-18 00:15:25 +0200

    Add SEO optimization files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    target: 'esnext',
+    assetsDir: 'assets',
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) return 'vendor';
+        }
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
+  }
 });
