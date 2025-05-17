0b3b06edd99348ded0e844425c71f6970cc768f7
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-18 00:20:53 +0200

    Fix CSS loading and file paths

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,15 @@ export default defineConfig({
     cssCodeSplit: true,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor';
-        }
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       }
     }
   },
