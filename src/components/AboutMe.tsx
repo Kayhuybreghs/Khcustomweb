7f9dc15c75ed7faa5379e9abb3492d094025b242
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-18 10:15:39 +0200

    Fix image paths and file structure

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -38,16 +38,14 @@ const AboutMe: React.FC = () => {
                   <img
                     src="/about-me/Aboutme-picture.webp"
                     alt="Kay Huybreghs - Webdesigner en eigenaar van KHCustomWeb"
-                    className="w-full h-full object-cover object-top"
+                    className="w-full h-full object-cover object-center"
                     width="256"
                     height="256"
                     loading="lazy"
                   />
                 </div>
                 <div className="mt-6 text-center">
-                  <p className="text-2xl font-bold text-orange-500 mb-4">4+ jaar ervaring in professioneel webdesign
-
- </p>
+                  <p className="text-2xl font-bold text-orange-500 mb-4">4+ jaar ervaring in professioneel webdesign</p>
                   <div className="flex justify-center gap-4">
                     <a
                       href="https://www.instagram.com/khcustomweb/?igsh=OWhndTZzYTlsNTR3&utm_source=qr"
