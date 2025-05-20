7931cb01f23070c19a4bd76c89ec2ab6ad4e999e
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-20 19:41:25 +0200

    Fix React hydration mismatch

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -5,13 +5,13 @@ import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
 
-ReactDOM.hydrateRoot(
-  document.getElementById('root')!,
-  <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
+// Remove React.StrictMode to prevent double rendering during development
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
 );
