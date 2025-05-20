12b58cefceddbc57d3bb4dc4cfb17411b6ac009b
kay huybreghs <kayhuybreghs@icloud.com>
2025-05-20 20:07:59 +0200

    Fix vite-plugin-ssr import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePluginSsr } from 'vite-plugin-ssr/plugin';
+import VitePluginSsr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
   plugins: [
